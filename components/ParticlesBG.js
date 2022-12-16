b516a704c922a861dd7d482054cde4735d0833e1
yash-bele <yashgbele@gmail.com>
2022-12-16 23:32:19 +0530

    commit

--- components/ParticlesBG.js
+++ components/ParticlesBG.js
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
+import { particlesData } from './ParticlesData';
 
 const ParticlesBG = () => {
   const particlesInit = useCallback(async (engine) => {
@@ -9,113 +10,10 @@ const ParticlesBG = () => {
 
   return (
     <Particles
-      className='absolute w-screen h-screen -z-50'
+      className='absolute -z-50'
       id='tsparticles'
       init={particlesInit}
-      options={{
-        fullScreen: {
-          enable: true,
-          zIndex: 1,
-        },
-        particles: {
-          number: {
-            value: 160,
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-          },
-          color: {
-            value: '#ffffff',
-          },
-          shape: {
-            type: 'circle',
-          },
-          opacity: {
-            value: 1,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0,
-              sync: false,
-            },
-          },
-          size: {
-            value: 3,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 4,
-              size_min: 0.3,
-              sync: false,
-            },
-          },
-          line_linked: {
-            enable: false,
-            distance: 150,
-            color: '#ffffff',
-            opacity: 0.4,
-            width: 1,
-          },
-          move: {
-            enable: true,
-            speed: 1,
-            direction: 'none',
-            random: true,
-            straight: false,
-            out_mode: 'out',
-            bounce: false,
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 600,
-            },
-          },
-        },
-        interactivity: {
-          events: {
-            onhover: {
-              enable: true,
-              mode: 'bubble',
-            },
-            onclick: {
-              enable: true,
-              mode: 'repulse',
-            },
-            resize: true,
-          },
-          modes: {
-            grab: {
-              distance: 400,
-              line_linked: {
-                opacity: 1,
-              },
-            },
-            bubble: {
-              distance: 250,
-              size: 0,
-              duration: 2,
-              opacity: 0,
-              speed: 3,
-            },
-            repulse: {
-              distance: 400,
-              duration: 0.4,
-            },
-            push: {
-              particles_nb: 4,
-            },
-            remove: {
-              particles_nb: 2,
-            },
-          },
-        },
-        retina_detect: true,
-        background: {
-          color: '#000000',
-        },
-      }}
+      options={particlesData}
     />
   );
 };
